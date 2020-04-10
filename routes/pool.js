0ee093fa978399a57b23d3cc734aced73c409cb7
kaushalhub <40087944+kaushalhub@users.noreply.github.com>
2020-04-10 15:09:39 +0530

    database add

--- routes/pool.js
+++ routes/pool.js
@@ -2,14 +2,14 @@ var mysql = require('mysql')
 
 const pool = mysql.createPool({
 
-  host: "localhost",
-  // host: "us-cdbr-iron-east-04.cleardb.net",
-  user: "root",
-  // user: "b2de5099507ea7",
-  password: "123",
-  // password: "13c7e046",
-  database: "pubgstartup",
-  // database: "heroku_2c7966fc0d4b1ba",
+  // host: "localhost",
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  // user: "root",
+  user: "b426c723a16136",
+  // password: "123",
+  password: "975ee915",
+  // database: "pubgstartup",
+  database: "heroku_684ab84efad05a0",
   port: "3306",
   connectionLimit: 100,
   multipleStatements: true
